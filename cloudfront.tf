7ad7d9b669931e19263185a075f1c324396571ba
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-20 22:16:10 +0000

    adds cloudfront distro

---
+++ cloudfront.tf
@@ -0,0 +1,48 @@
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_lb.frontend_lb.dns_name
+    origin_id   = "frontendweb"
+
+    custom_origin_config {
+      origin_protocol_policy = "https-only"
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "frontendweb"
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  depends_on = [aws_lb.frontend_lb]
+}
