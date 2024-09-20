1db79a098bd0e1e399f9f5e6857fb64ae66940d2
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-20 22:17:55 +0000

    removes ssh from lb_sg

--- loadbalancersg.tf
+++ loadbalancersg.tf
@@ -6,14 +6,6 @@ resource "aws_security_group" "lbsecuritygroupB" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 0
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   ingress {
     from_port   = 3000
     to_port     = 3000
