9f87b64792e1e60a29eaa4b9669609c1d503aa47
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-20 21:09:52 +0000

    refactors vpc to look simple

--- vpc.tf
+++ vpc.tf
@@ -7,9 +7,10 @@ resource "aws_vpc" "cloudforce_vpc" {
   }
 }
 
+# public subnets
 resource "aws_subnet" "public_subnets" {
   count                   = length(var.public_subnet_cidrs)
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id                  = aws_vpc.cloudforce_vpc.id
   cidr_block              = element(var.public_subnet_cidrs, count.index)
   map_public_ip_on_launch = true
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
@@ -18,9 +19,10 @@ resource "aws_subnet" "public_subnets" {
   }
 }
 
+# private subnets
 resource "aws_subnet" "private_subnets" {
   count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.eks_vpc.id
+  vpc_id            = aws_vpc.cloudforce_vpc.id
   cidr_block        = element(var.private_subnet_cidrs, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
   tags = {
@@ -28,16 +30,18 @@ resource "aws_subnet" "private_subnets" {
   }
 }
 
+
+# internet gateway
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.eks_vpc.id
+  vpc_id = aws_vpc.cloudforce_vpc.id
   tags = {
     Name = "${var.environment_name}-igw"
   }
 }
 
-
+#pulic route table
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
+  vpc_id = aws_vpc.cloudforce_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -52,3 +56,5 @@ resource "aws_route_table_association" "public_rt_association" {
   subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
   route_table_id = aws_route_table.public_route_table.id
 }
+
+data "aws_availability_zones" "available" {}
