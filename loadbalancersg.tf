82ee2197cadb4cf878c461beee67a27044efdd1a
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-21 06:29:08 +0000

    refining security group definitions for both frontend and backend loadbalancers

--- loadbalancersg.tf
+++ loadbalancersg.tf
@@ -1,21 +1,41 @@
-resource "aws_security_group" "lbsecuritygroupB" {
-  name = "lbsecuritygroupB"
+#FrontendLB security group
+resource "aws_security_group" "frontendLB_sg" {
+  name = "frontendLB-sg"
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ 
 
   egress {
     from_port   = 0
     to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = aws_vpc.cloudforce_vpc.id
+
+}
+
+
+#BackendLB security group
+resource "aws_security_group" "backendLB_sg" {
+  name = "backendLB-sg"
+  ingress {
+    from_port   = 0
+    to_port     = 80
+    security_groups = [aws_security_group.frontend_sg.id]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    security_groups = [aws_security_group.backend_sg.id]
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
