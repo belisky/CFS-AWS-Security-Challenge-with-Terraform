7ef23f98264aace48396ef85e7c4dbb78672e2ad
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-22 14:31:36 +0000

    refines terraform output

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "frontend_dns" {
-  value = aws_lb.frontend_lb
+  value = aws_lb.frontend_lb.dns_name
 }
 output "cloudfront_dns" {
-  value = aws_cloudfront_distribution.cdn
+  value = aws_cloudfront_distribution.cdn.domain_name
 }
