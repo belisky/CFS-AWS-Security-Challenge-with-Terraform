fe8ed101e6e492b0c7ebcc548f99586b4592f96f
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-22 20:51:10 +0000

    removes s3 config from cloudtrail

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -8,25 +8,6 @@ resource "aws_cloudtrail" "cloudforce_trail" {
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket" "cloudforce" {
-  bucket        = var.cloudforce_trail
-  force_destroy = true
-   
-}
-resource "aws_s3_bucket_ownership_controls" "restrict" {
-  bucket = aws_s3_bucket.cloudforce.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "restrict" {
-  depends_on = [aws_s3_bucket_ownership_controls.restrict]
-
-  bucket = aws_s3_bucket.cloudforce.id
-  acl    = "private"
-}
- 
 data "aws_iam_policy_document" "CF_policy-cloudtrail" {
   statement {
     sid    = "AWSCloudTrailAclCheck"
@@ -83,16 +64,6 @@ data "aws_iam_policy_document" "CF_policy-cloudtrail" {
   }
 }
 
-resource "aws_s3_bucket_policy" "cloudforce_policy" {
-  bucket = aws_s3_bucket.cloudforce.id
-  policy = data.aws_iam_policy_document.CF_policy-cloudtrail.json
-}
 
-data "aws_caller_identity" "current" {}
-
-data "aws_partition" "current" {}
-
-data "aws_region" "current" {}
- 
 
  
