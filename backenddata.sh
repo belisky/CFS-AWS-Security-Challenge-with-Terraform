23925c6108d68fd6f4aff45c46f6a7a80bfc37f8
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-22 19:52:46 +0000

    removes sudo from user-data script

--- backenddata.sh
+++ backenddata.sh
@@ -2,7 +2,7 @@
 # Install docker
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -11,5 +11,5 @@ apt-get update
 apt-get install -y docker-ce
 usermod -aG docker ubuntu
 # pull backend image from dockerhub
-sudo docker pull marcrine/bookappbackend
-sudo docker run -d -p 80:5000 marcrine/bookappbackend
+docker pull marcrine/bookappbackend
+docker run -d -p 80:5000 marcrine/bookappbackend
--- frontenddata.sh
+++ frontenddata.sh
@@ -2,7 +2,7 @@
 # Install docker
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -11,5 +11,5 @@ apt-get update
 apt-get install -y docker-ce
 usermod -aG docker ubuntu
 # pull frontend image from dockerhub
-sudo docker pull marcrine/bookappfrontend
-sudo docker run -d -p 80:3000 marcrine/bookappfrontend
+docker pull marcrine/bookappfrontend
+docker run -d -p 80:3000 marcrine/bookappfrontend
